7b351d2bb1643be28a8428530fb9e7d71d33c05c
MuhsinAliS <145312385+MuhsinAliS@users.noreply.github.com>
2023-12-18 18:00:01 +0700

    push to Github

---
+++ resources/sesi/index.blade.php
@@ -0,0 +1,13 @@
+<h1>login</h1>
+<form action="/sesi/login" method="post">
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Email</label>
+        <input type="password" name="password" class="form-control">
+    </div>
+    <div class="mb-3 d-grid">
+        <button name="submit" type="submit" class="btn-btn-primary">Login</button>
+</form>
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\SessionController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/sesi',[SessionController::class,'index']);
+Route::post('/sesi/login',[SessionController::class,'login']);
